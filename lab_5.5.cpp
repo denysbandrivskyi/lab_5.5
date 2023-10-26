2d8939fd98e37649024531a74d429162d6d250d5
denysbandrivskyi <denys.bandrivskyi.ri.2023@lpnu.ua>
2023-10-26 11:06:52 +0300

    first commit

---
+++ lab_5.5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int sumOfDigits(int n, int& depth) {
+    // Умова закінчення рекурсії
+    if (n == 0) {
+        return 0;
+    }
+    // Рекурентний виклик функції для обчислення суми цифр
+    depth++;
+    return n % 10 + sumOfDigits(n / 10, depth);
+}
+
+int main() {
+    int number;
+    cout << "n: ";
+    cin >> number;
+
+    int depth = 0;
+    int result = sumOfDigits(number, depth);
+
+    cout << "Sum " << number << " = " << result << endl;
+    cout << "depth: " << depth << endl;
+
+    return 0;
+}
